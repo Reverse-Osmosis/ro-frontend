787110234343bb516c096e7127a8e607560a9d5d
Jose Ospina <joseospina@Joses-MacBook-Pro.local>
2022-10-02 17:45:15 -0500

    ( Layout )[ PopUp ]: Setup PopUp Layout (ROF-1)

--- pages/home.tsx
+++ pages/home.tsx
@@ -3,32 +3,37 @@ import Card from "../src/components/card";
 import Footer from "../src/components/footer";
 import Header from "../src/components/header";
 import Layout from "../src/components/layout";
+import {PopUpAtom} from "../src/components/ui/PopUpAtom/PopUp.atom";
 
 function Home() {
-  return (
-    <Layout>
-      {/* <div className=" "> */}
-      {/* <Header />
+    return (
+        <>
+            <Layout>
+                {/* <div className=" "> */}
+                {/* <Header />
       <Footer /> */}
-      <>
-        <div className="flex flex-col items-center my-28 ">
-          <h1>WELCOME TO REVERSE OSMOSIS</h1>
-          <p className="text-center">
-            New <span className="font-bold">projects</span>
-            <span className="block"> are available.</span>
-          </p>
-        </div>
-        <div className="flex flex-wrap justify-center gap-10 mb-28">
-          {Array(8)
-            .fill(1)
-            .map((item, index) => (
-              <Card key={index} />
-            ))}
-        </div>
-      </>
-      {/* </div> */}
-    </Layout>
-  );
+                <>
+                    <div className="flex flex-col items-center my-28 ">
+                        <h1>WELCOME TO REVERSE OSMOSIS</h1>
+                        <p className="text-center">
+                            New <span className="font-bold">projects</span>
+                            <span className="block"> are available.</span>
+                        </p>
+                    </div>
+                    <div className="flex flex-wrap justify-center gap-10 mb-28">
+                        {Array(8)
+                        .fill(1)
+                        .map((item, index) => (
+                            <Card key={index}/>
+                        ))}
+                    </div>
+                </>
+                {/* </div> */}
+            </Layout>
+            <PopUpAtom/>
+        </>
+
+    );
 }
 
 export default Home;
